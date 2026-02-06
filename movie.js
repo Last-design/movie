f2c91b2d90070caad5225deb6c0395e7ae622aea
Last-design <lastshyaka@gmail.com>
2026-02-06 13:11:35 +0200

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,23 @@
+const movies = [
+{
+id: 1,
+title: "Lilo & Stitch",
+poster: "[https://i.imgur.com/8Q1ZqKQ.jpeg](https://i.imgur.com/8Q1ZqKQ.jpeg)",
+trailer: "[https://www.youtube.com/watch?v=dQw4w9WgXcQ](https://www.youtube.com/watch?v=dQw4w9WgXcQ)",
+reviews: ["Great movie!", "Loved the animation!", "Family favorite!"]
+},
+{
+id: 2,
+title: "Spider-Man",
+poster: "[https://i.imgur.com/3fJ1vL8.jpeg](https://i.imgur.com/3fJ1vL8.jpeg)",
+trailer: "[https://www.youtube.com/watch?v=dQw4w9WgXcQ](https://www.youtube.com/watch?v=dQw4w9WgXcQ)",
+reviews: ["Amazing action scenes", "Good plot", "Peter Parker rocks!"]
+},
+{
+id: 3,
+title: "Avengers: Endgame",
+poster: "[https://i.imgur.com/uB0mZpZ.jpeg](https://i.imgur.com/uB0mZpZ.jpeg)",
+trailer: "[https://www.youtube.com/watch?v=dQw4w9WgXcQ](https://www.youtube.com/watch?v=dQw4w9WgXcQ)",
+reviews: ["Epic conclusion!", "Loved the battles!", "Emotional!"]
+}
+];
